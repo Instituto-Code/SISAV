55126057e29b9eb25ea9875e735982a8c120679d
Lucas Paulo <lp152659@gmail.com>
2026-02-20 18:18:46 -0300

    feat: Minddleware de erros

--- server/src/common/infra/http/app.ts
+++ server/src/common/infra/http/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes.js";
+import { ErrorHandler } from "./error-handler.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api", router);
+app.use(ErrorHandler);
 
 export { app };
---
+++ server/src/common/infra/http/error-handler.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../../domain/errors/app-errors";
+
+export function ErrorHandler(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+){
+    if(err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message
+        });
+    }
+
+    console.log(err);
+
+    return res.status(500).json({
+        message: "Internal server error."
+    })
+}
